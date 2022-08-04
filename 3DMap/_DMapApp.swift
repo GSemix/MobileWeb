89d41cc7eec6dc78d21c51fc2faad7fbfe8d27ed
GSemix <63607553+GSemix@users.noreply.github.com>
2022-08-04 20:21:24 +0300

    Initial Commit

---
+++ 3DMap/_DMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DMapApp.swift
+//  3DMap
+//
+//  Created by Семен Безгин on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct _DMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
